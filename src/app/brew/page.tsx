545a5eb1e76ac379099bd784b255d1dc89497267
Robin Gagnon <me@reobin.dev>
2023-12-23 22:57:17 -0500

    Add lock buttons for inputs

--- src/app/brew/page.tsx
+++ src/app/brew/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChangeEvent } from 'react';
+import { ChangeEvent, Fragment, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import BrewRatio from '@/lib/types/brewRatio';
@@ -9,11 +9,21 @@ import NumberHelper from '@/helpers/number';
 import styles from './brew.module.css';
 
 function BrewPage() {
+  const [lockMap, setLockMap] = useState<Record<keyof BrewRatio, boolean>>({
+    coffeeInput: false,
+    coffeeOutput: false,
+    ratio: false,
+  });
+
+  const isLocked = Object.values(lockMap).filter(Boolean).length > 1;
+
   const { register, watch, setValue } = useForm<BrewRatio>();
 
   const coffeeInput = watch('coffeeInput');
   const coffeeOutput = watch('coffeeOutput');
   const ratio = watch('ratio');
+
+  console.log({ coffeeInput, coffeeOutput, ratio });
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value: valueAsString } = event.target;
@@ -22,36 +32,36 @@ function BrewPage() {
 
     switch (name) {
       case 'coffeeInput':
-        if (coffeeOutput != null) {
+        if (coffeeOutput != null && !lockMap.ratio) {
           setValue('ratio', NumberHelper.round(coffeeOutput / value, 2));
           return;
         }
 
-        if (ratio != null) {
-          setValue('coffeeOutput', NumberHelper.round(value / ratio, 2));
+        if (ratio != null && !lockMap.coffeeOutput) {
+          setValue('coffeeOutput', NumberHelper.round(value * ratio, 2));
           return;
         }
 
         break;
       case 'coffeeOutput':
-        if (coffeeInput != null) {
+        if (coffeeInput != null && !lockMap.ratio) {
           setValue('ratio', NumberHelper.round(value / coffeeInput, 2));
           return;
         }
 
-        if (ratio != null) {
+        if (ratio != null && !lockMap.coffeeInput) {
           setValue('coffeeInput', NumberHelper.round(value * ratio, 2));
           return;
         }
 
         break;
       case 'ratio':
-        if (coffeeInput != null) {
+        if (coffeeInput != null && !lockMap.coffeeOutput) {
           setValue('coffeeOutput', NumberHelper.round(coffeeInput * value, 2));
           return;
         }
 
-        if (coffeeOutput != null) {
+        if (coffeeOutput != null && !lockMap.coffeeInput) {
           setValue('coffeeInput', NumberHelper.round(coffeeOutput / value, 2));
           return;
         }
@@ -60,31 +70,55 @@ function BrewPage() {
     }
   }
 
+  function isInputDisabled(input: keyof BrewRatio): boolean {
+    return lockMap[input] || isLocked;
+  }
+
+  function isLockButtonDisabled(
+    input: keyof BrewRatio,
+    value: number,
+  ): boolean {
+    return (isLocked && !lockMap[input]) || value == null || isNaN(value);
+  }
+
+  function getLockButtonLabel(input: keyof BrewRatio): string {
+    return isInputDisabled(input) ? 'Unlock' : 'Lock';
+  }
+
+  function renderInput(input: keyof BrewRatio) {
+    return (
+      <Fragment>
+        <input
+          type="number"
+          step="0.01"
+          {...register(input, { valueAsNumber: true, onChange })}
+          className={styles.input}
+          disabled={isInputDisabled(input)}
+        />
+        <button
+          type="button"
+          onClick={() => setLockMap({ ...lockMap, [input]: !lockMap[input] })}
+          disabled={isLockButtonDisabled(input, coffeeInput)}
+        >
+          {getLockButtonLabel(input)}
+        </button>
+      </Fragment>
+    );
+  }
+
   return (
     <form className={styles.container}>
-      <label>
+      <label className={styles.inputGroup}>
         coffee input:
-        <input
-          type="number"
-          step="0.01"
-          {...register('coffeeInput', { valueAsNumber: true, onChange })}
-        />
+        {renderInput('coffeeInput')}
       </label>
-      <label>
+      <label className={styles.inputGroup}>
         coffee output:
-        <input
-          type="number"
-          step="0.01"
-          {...register('coffeeOutput', { valueAsNumber: true, onChange })}
-        />
+        {renderInput('coffeeOutput')}
       </label>
-      <label>
+      <label className={styles.inputGroup}>
         ratio: 1:
-        <input
-          type="number"
-          step="0.01"
-          {...register('ratio', { valueAsNumber: true, onChange })}
-        />
+        {renderInput('ratio')}
       </label>
     </form>
   );
